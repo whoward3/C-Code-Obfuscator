b4a47ae5efebf967bfaed46a4e4c484affb20a5c
whoward3 <whoward3@uwyo.edu>
2020-05-04 16:33:13 -0600

    Initial Commit

---
+++ obfuscator/obfuscator.py
@@ -0,0 +1,13 @@
+"""
+obfuscator
+Description: A basic python script to aid in the obfuscation of c and c++ source code files
+Authors: Sam "Alice" Blair, Winston Howard, Chance Sweetser
+Created Date: 05/04/20
+"""
+
+def main():
+    """
+    The main function to begin the obfuscation of c code files
+    """
+
+ 
