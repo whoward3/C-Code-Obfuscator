ec040aed7f02cc300731536f197f6d1eb6a463e3
Chance Sweetser <chance.sweetser@outlook.com>
2020-05-12 16:53:35 -0600

    update hellotest

--- tests/hello-test.cpp
+++ tests/hello-test.cpp
@@ -2,22 +2,12 @@
 
 #include <string>
 #include <iostream>
-using namespace std;
 
-//This is a dummy function
-int main()
-{
-    int number = 5;
-    string res = hello_function(number);
-    cout << res;
-    return 0; /*End of the function*/
-}
-
-string hello_function(int a)
+std::string hello_function(int a)
 {
     while (a > 0)
     {
-        cout << a;
+        std::cout << a;
         a = a - 1;
     }
 
@@ -30,3 +20,13 @@ string hello_function(int a)
         return "Goodbye World";
     }
 }
+
+//This is a dummy function
+int main()
+{
+    int number = 5;
+    std::string res = hello_function(number);
+    std::cout << res;
+    return 0; /*End of the function*/
+}
+
